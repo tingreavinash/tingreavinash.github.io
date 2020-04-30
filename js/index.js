6baa1886b0f3878fdd56f235423dfe0c5617a587
Avinash Tingre <39552348+tingreavinash@users.noreply.github.com>
2020-05-01 00:39:58 +0530

    Add dynamic experience

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function totalDays(){
+	var minutes = 1000*60;
+    var hours = minutes*60;
+    var days = hours*24;
+    var months = days*30;
+    var years = months*12;
+
+    var d1=new Date(2017,10,3);//Remember, months are 0 based in JS
+	var d2=new Date();
+
+	var diff_days = Math.round((d2-d1)/days);
+
+	return diff_days;
+}
+function printYear(){
+	var diff_days = totalDays();
+	var res_year = Math.floor(diff_days/(30*12));
+	return res_year;
+
+}
+
+function printMonth(){
+	var diff_days = totalDays();
+	var res_month = Math.floor((diff_days/30)%12)+1;
+	console.log('Total Experience: '+printYear()+' Years '+res_month+' Months');
+	return res_month;
+}
