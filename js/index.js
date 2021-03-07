3a2fb61f4f1004ccbdf8dfad2e4505c571441cde
Avinash Tingre <tingre.avinash@gmail.com>
2021-03-07 16:34:42 +0530

    add person svg on home

--- js/index.js
+++ js/index.js
@@ -26,3 +26,51 @@ function printMonth(){
 	return res_month;
 }
 
+function populatedata(imgSrc, certUrl){
+    document.getElementById("cert_img_field").src = imgSrc;
+    document.getElementById("cert_url_field").href = certUrl;
+    console.log("model is opened");
+  }
+
+$(window).on('load',function(){
+    $('#myModal').modal('show');
+});
+
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+function showGreeting(){
+    var myDate = new Date();
+    var hrs = myDate.getHours();
+    var greet;
+
+    if (hrs < 12){
+    	greet = 'Good Morning! &#127773;';
+    	msg = 'Nothing is impossible when you put your mind, heart, soul, and sweat into it. Have a great morning!';
+    }
+    else if (hrs >= 12 && hrs <= 17){
+    	greet = 'Good Afternoon! &#127774;';
+    	msg = 'Every experience, No matter how bad it seems, holds within it a blessing of some kind. The goal is to find it.';
+    }
+    else if (hrs >= 17 && hrs <= 24){
+        greet = 'Good Evening! &#127770;';
+    	msg = 'Evenings are life\'s way of saying that you are closer to your dreams.';
+    }
+    document.getElementById('greetingMessage').innerHTML =
+        'Hey, <b>' + greet + '</b>';
+
+    document.getElementById('customWish').innerHTML =
+            '' + msg + '';
+
+}
+
+
+$(function () {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out-back'
+    });
+})
