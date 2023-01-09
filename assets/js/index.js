aad784f7147ec094700cec1ab81c726bbe428c3c
Avinash Tingre <tingre.avinash@gmail.com>
2023-01-09 15:36:37 +0530

    Add camunda certificates

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,7 +11,7 @@ function getTotalExpInDays() {
     const noOfMilliSecInDay = 24 * 60 * 60 * 1000;
 
     const careerStartDate = new Date(2017, 09, 3); //months are 0 based in JS
-    console.log("%cCareer started on: ", 'background: #222; color: #bada55', careerStartDate);
+    //console.log("%cCareer started on: ", 'background: #222; color: #bada55', careerStartDate);
 
     const todaysDate = new Date();
     const totalExperienceInDays = Math.round((todaysDate - careerStartDate) / noOfMilliSecInDay);
@@ -41,14 +41,43 @@ function calculateTotalExperience() {
 
 }
 
-function fillCertificateData(imgSrc, certUrl, isValidateOptionHidden) {
-    console.log("isValidateOptionHidden: " + isValidateOptionHidden);
-    if (isValidateOptionHidden == true) {
+function fillCertificateData(imgSrc, certUrl, isValidateButtonVisible) {
+    console.log("isValidateButtonVisible: " + isValidateButtonVisible);
+    if (isValidateButtonVisible == false) {
         document.getElementById("cert_url_field").style.display = "none";
     }
     document.getElementById("cert_img_field").src = imgSrc;
     document.getElementById("cert_url_field").href = certUrl;
     console.log("Certificate modal opened");
+}
+
+function fillCertificateDataCarousel(isValidateButtonVisible, ...certificateUrls) {
+
+    var carouselIndicators = "";
+
+    for (var i = 0; i < certificateUrls.length; i++) {
+        console.log("URL: " + certificateUrls[i]);
+        carouselIndicators += "<li data-target='#carouselImageIndicators' data-slide-to='" + i + "'";
+        if (i == 0) {
+            carouselIndicators += " class='active'";
+        }
+        carouselIndicators += " ></li>";
+    }
+
+    document.getElementById("carousel-indicators").innerHTML = carouselIndicators;
+
+    var carouselInners = "";
+
+    for (var i = 0; i < certificateUrls.length; i++) {
+        carouselInners += "<div class='carousel-item ";
+        if (i == 0) {
+            carouselInners += "active";
+        }
+        carouselInners += "'><img class='cert_img' src='" + certificateUrls[i] + "' alt='Certificate' /></div>";
+    }
+
+    document.getElementById("carousel-inner").innerHTML = carouselInners;
+
 }
 
 function activateTooltips() {
